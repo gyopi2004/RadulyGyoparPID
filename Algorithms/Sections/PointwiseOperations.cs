4a7d312fdaaf43cb99455195d0ff906c2ae56b97
gyopi2004 <gyoparraduly@yahoo.com>
2026-03-03 10:58:32 +0200

    initial

---
+++ Algorithms/Sections/PointwiseOperations.cs
@@ -0,0 +1,79 @@
+﻿using Algorithms.Utilities;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Algorithms.Sections
+{
+    public class PointwiseOperations
+    {
+        public static byte[] CreareLinearOpLUT(float alpha, float beta)
+        {
+            byte[] table = new byte[256];
+            for (int r = 0; r < 256; r++)
+            {
+                table[r] = Utils.Clamp(alpha * r + beta);
+            }
+            return table;
+        }
+
+        public static byte[] CreateGammaOpLUT(double gamma)
+        {
+            byte[] table = new byte[256];
+            double c = (float)Math.Pow(255.0, 1 - gamma);
+            for (int r = 0; r < 256; r++)
+            {
+
+                table[r] = Utils.Clamp((float)(c * Math.Pow(r, gamma)));
+            }
+            return table ;
+        }
+
+        public static Image<Gray, byte> ApplyLut(Image<Gray, byte> inputImage, byte[] lut)
+        {
+            Image<Gray, byte> result = new Image<Gray, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    byte r = inputImage.Data[y, x, 0];
+                    byte s = lut[r];
+                    result.Data[y, x, 0] = s;
+                }
+            }
+                return result;
+             }
+
+        public static Image<Bgr, byte> ApplyLut(Image<Bgr, byte> inputImage, byte[] lut)
+        {
+            Image<Bgr, byte> result = new Image<Bgr, byte>(inputImage.Size);
+
+            for (int y = 0; y < inputImage.Height; ++y)
+            {
+                for (int x = 0; x < inputImage.Width; ++x)
+                {
+                    for (byte c = 0; c < 3; c++)
+                    {
+                        byte r = inputImage.Data[y, x, c];
+                        byte s = lut[r];
+                        result.Data[y, x, c] = s;
+                    }
+
+                }
+            }
+                return result;
+            
+        }
+
+        
+
+
+        
+    }
+}
---
+++ Algorithms/Utilities/Utils.cs
@@ -0,0 +1,53 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Threading.Tasks;
+
+namespace Algorithms.Utilities
+{
+    public class Utils
+    {
+        #region Compute histogram
+        public static int[] ComputeHistogram(Image<Gray, byte> inputImage)
+        {
+            int[] histogram = new int[256];
+
+            Parallel.For(0, inputImage.Height,
+        () => new int[256],   // histogram local
+        (y, state, localHist) =>
+        {
+            for (int x = 0; x < inputImage.Width; ++x)
+            {
+                localHist[inputImage.Data[y, x, 0]]++;
+            }
+            return localHist;
+        },
+        localHist =>
+        {
+            lock (histogram)
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    histogram[i] += localHist[i];
+                }
+            }
+        });
+
+            return histogram;
+        }
+        #endregion
+
+        public static byte Clamp(float x)
+        {
+            if (x < 0)
+            {
+                return 0;
+            }
+
+            else if (x > 255)
+            {
+                return 255;
+            }
+            return (byte)(x + 0.5);
+        }
+    }
+}
